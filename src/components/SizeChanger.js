70736b767079d61044b6c754cd8ed62bc74c4908
Nick <nhunsaker20@gmail.com>
2020-01-29 12:22:56 -0700

    finished

--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 export default class ColorChanger extends Component {
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={event => this.props.update(+event.target.value)} disabled={
+        this.props.allowEdit === 'false'}>
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
